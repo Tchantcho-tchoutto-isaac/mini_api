fdad163edf7688d8292706c9641ac40c48386fd8
Tchantcho-tchoutto-isaac <tchantchoisaac1997@gmail.com>
2023-08-20 23:50:42 +0200

    mise à jour des donnée

---
+++ mettre_a_jour_par_id.php
@@ -0,0 +1,37 @@
+<?php 
+header('content_type:application/json');
+require_once 'connection.php';
+
+$reponse=array();
+//les element à mettre à jour :box office ,genre,langue,date de sortie,nombre d'étoiles.
+
+if(isset($_POST['id']) && isset($_POST['description'])&& isset($_POST['nombre_etoile'])
+&& isset($_POST['box_office'])){
+    //success
+    $id=$_POST['id'];
+    $description=$_POST['description'];
+    $nombre_etoile=$_POST['nombre_etoile'];
+    $box_office=$_POST['box_office'];
+
+    $requete=$con->prepare("UPDATE films SET description='$description', nombre_etoile='$nombre_etoile'
+    box_office='$box_office' WHERE id ='$id'");
+
+    if($requete->execute()){
+        $reponse['error']= "false";
+        $reponse['message']= "mise à jour effectuer avec succès";
+    }
+    else{
+
+        $reponse['error']= "true";
+        $reponse['message']= "impossible de mettre a jour les elements renseigner";
+    }
+
+}
+else{
+    $reponse['error']= "true";
+        $reponse['message']= "renseiger les element a mettre a jour";
+}
+
+print_r($reponse)
+
+?>
---
+++ test_mise_a_jour.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <form  method="POST" action="http://localhost/mini_api/mettre_a_jour_par_id.php"  >
+        <input type="text" name="id" value="1">
+        <input type="text" name="description" value="nouvelle description du films ">
+        <input type="text" name="nombre_etoile" value="9">
+        <input type="text" name="box_office" value="76002">
+        <input type="Submit" value="mettre à jour">
+    </form>
+</div>
+</body>
+</html>
