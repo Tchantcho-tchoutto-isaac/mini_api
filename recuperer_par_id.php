2a4a4847a1d2b5aeb6778f013c38c1c4c3dc433e
Tchantcho-tchoutto-isaac <tchantchoisaac1997@gmail.com>
2023-08-20 22:43:02 +0200

    recuperer_par_id
    recupérer les films de la bd par l'id

---
+++ recuperer_par_id.php
@@ -0,0 +1,51 @@
+<?php
+header('content_type:application/json');
+require_once 'connection.php';
+
+$reponse=array();
+
+if(isset($_GET['id'])){ 
+
+$titre=$_GET['id'];
+$query=$con->prepare("SELECT id,titre,description,langue,genre,date_de_sortie,box_office,duree,nombre_etoile 
+FROM films WHERE id=?");
+$query->bind_param("i",$id);
+
+//verifier le resultat de notre requette 
+if ($query->execute()) {
+    //La requte a bien été executée
+    $query->bind_result($id,$titre,$description,$langue,$genre,$date_de_sortie,$obx_office,
+    $duree,$nombre_etoile);
+    $query->fetch();
+
+    $films=array(
+        'id'=>$id,
+        'titre'=>$titre,
+        'description'=>$description,
+        'langue'=>$langue,
+        'genre'=>$genre,
+        'date_de_sortie'=>$date_de_sortie,
+        'obx_office'=>$obx_office,
+        'duree'=>$duree,
+        'nombre_etoile'=>$nombre_etoile,
+    );
+    
+    $repoonse['error']=false;
+    $reponse['films']=$films;
+    $reponse['message']="le films rechercher est dans la base de donnée";
+    $query->close();
+     print_r($reponse);
+}else{
+    //impossible d'executer cette requette 
+    $repoonse['error']=true;
+    $reponse['message']="le films rechercher n'existe pas dans la base de donnée";
+}
+
+
+ }else { 
+    $repoonse['error']=true;
+    $reponse['message']="veuillez fournir l_id du  films a rechercher";
+
+ }
+ echo json_encode($reponse);
+?>
