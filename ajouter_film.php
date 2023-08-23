018518a05961efe02866a8e67b0e6b0e2750c580
Tchantcho-tchoutto-isaac <tchantchoisaac1997@gmail.com>
2023-08-23 21:44:05 +0200

    ajout d'un film  du mini projet
    ajout d'un films dans la base de donnée .

--- ajouter_film.php
+++ ajouter_film.php
@@ -1,38 +1,38 @@
 <?php
-header('content_type:application/json');
+header('Content-Type: application/json');
 require_once 'connection.php';
 
-$reponse=array();
-//les differents entrées: titre,description,langue,genre,date_sortie,box_office,duree et nombre d'étoile
+$response = array();
 
-if (isset($_POST['titre'])&& isset($_POST['description']) &&
- isset($_POST['langue']) && isset($_POST['genre'])&& isset($_POST['date_de_sortie']) 
- && isset($_POST['box_office'])&& isset($_POST['nombre_etoile']) && isset($_POST['duree']) )
+if (isset($_POST['titre']) && isset($_POST['description']) &&
+    isset($_POST['langue']) && isset($_POST['genre']) && isset($_POST['date_de_sortie']) &&
+    isset($_POST['box_office']) && isset($_POST['nombre_etoile']) && isset($_POST['duree']))
 {
-   $titre = $_POST['titre'] ;
-   $description = $_POST['description'] ;
-   $genre = $_POST['genre'] ;
-   $date_de_sorti = $_POST['date_de_sorti'] ;
-   $box_office = $_POST['box_office'] ;
-   $duree = $_POST['duree'] ;
-   $nombre_etoile = $_POST['nombre_etoile'] ;
-   $requete = $con->prepare("INSERT INTO films(titre,description,langue,genre,date_de_sortie,box_office,duree,nombre_d'etoile) 
-   VALUE (?,?,?,?,?,?,?,?)");
-   $requete->bind_param('sssssdsd',$titre,$descriptionp, $langue, $genre,$date_sorti,
-   $box_office,$duree,$nombre_etoile);
-   if ($requete->execute()) {
-    //si success 
-    $reponse['error']=false;
-    $reponse['message']="Nouveau film ajouté avec succès";
-      }else {
-        $reponse['error']=true;
-        $reponse['message']="le film n'a pas été  ajouté";
-      }
-}else {
-    $reponse['error']=true;
-    $reponse['message']="veuillez renseigner les champ à ajouter";
+    $titre = $_POST['titre'];
+    $description = $_POST['description'];
+    $langue = $_POST['langue'];
+    $genre = $_POST['genre'];
+    $date_de_sortie = $_POST['date_de_sortie'];
+    $box_office = $_POST['box_office'];
+    $duree = $_POST['duree'];
+    $nombre_etoile = $_POST['nombre_etoile'];
+
+    $requete = $con->prepare("INSERT INTO films (titre, description, langue, genre, date_de_sortie, box_office, duree, nombre_etoile) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    
+    $requete->bind_param('sssssdss', $titre, $description, $langue, $genre, $date_de_sortie, $box_office, $duree, $nombre_etoile);
+
+    if ($requete->execute()) {
+        $response['error'] = false;
+        $response['message'] = "Nouveau film ajouté avec succès";
+    } else {
+        $response['error'] = true;
+        $response['message'] = "Le film n'a pas été ajouté";
+    }
+} else {
+    $response['error'] = true;
+    $response['message'] = "Veuillez renseigner tous les champs a ajouter";
 }
 
-print_r($reponse);
-
+echo json_encode($response);
 ?>
--- test_ajout.php
+++ test_ajout.php
@@ -9,12 +9,12 @@
 <form action="http://localhost/mini_api/ajouter_film.php" method="POST">
     <input type="text" name="titre" value="CASA DE PAPEL"/>
     <input type="text" name="description" value="Le meilleur film de braquage "/>
-    <input type="text" name="Langue" value="Fr"/>
+    <input type="text" name="langue" value="Fr"/>
     <input type="text" name="genre" value="Action"/>
     <input type="text" name="date_de_sortie" value="2023"/>
     <input type="text" name="box_office" value="1500000"/>
-    <input type="text" name="duree" value="167min"/>
-    <input type="text" name="nombre_etoile" value="9,8"/>
+    <input type="text" name="duree" value="167"/>
+    <input type="text" name="nombre_etoile" value="9"/>
     <input type="submit" value="ajouter">
 
 </form>
